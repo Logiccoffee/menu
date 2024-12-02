afadc62923dcb6139fdfc3ead216e07afa97a3b6
balqisrosa <balqisrosasekamayang@gmail.com>
2024-12-02 15:44:17 +0700

    logout

--- script.js
+++ script.js
@@ -477,3 +477,9 @@ function getLastPathSegment() {
   // Ambil bagian terakhir dari URL
   return parts[parts.length - 1];
 }
+
+function logout() {
+  document.cookie = "login=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+  localStorage.removeItem("token");
+  window.location.href = "http://logiccoffee.id.biz.id/";
+}
